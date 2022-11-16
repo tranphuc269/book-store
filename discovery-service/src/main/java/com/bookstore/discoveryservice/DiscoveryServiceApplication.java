95aa1e32905312c2932077ef2788ff1aa7cd40de
tranphuc269 <phuc260900@gmail.com>
2022-11-17 00:30:59 +0700

    build ci remote

--- discovery-service/src/main/java/com/bookstore/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/bookstore/discoveryservice/DiscoveryServiceApplication.java
@@ -3,11 +3,17 @@ package com.bookstore.discoveryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class DiscoveryServiceApplication {
-
+	@GetMapping("/heal-check")
+	public String check(){
+		return "This is discovery-service";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
